bb9f1d38e0acd9d2a18f0b00ec0aae0cac02be72
Jasmynaa <1656759947@qq.com>
2020-10-17 20:55:15 +0800

    提交work2

---
+++ precache-manifest.b3dfdf529efbd39f7de657a53c2fa5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491a757ce69b7922d4a19c2fe1a1b242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f8b82558496b26b78b",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "40b8947c93894035c87c",
+    "url": "/static/css/main.1e137239.chunk.css"
+  },
+  {
+    "revision": "e9f8b82558496b26b78b",
+    "url": "/static/js/2.c9b0e73c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9b0e73c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b8947c93894035c87c",
+    "url": "/static/js/main.52d24aa5.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
